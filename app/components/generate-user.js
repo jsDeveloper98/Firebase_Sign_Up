6a5f446fad7514eaffe1f77f584170346a4ddbc6
Hovhannes98 <Hovhannes98@gmail.com>
2020-09-10 16:24:41 +0400

    authentication with Hovo

--- app/components/generate-user.js
+++ app/components/generate-user.js
@@ -2,25 +2,25 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import {tracked} from "@glimmer/tracking";
 import { inject as service } from '@ember/service';
-
+import { set } from '@ember/object';
 
 export default class GenerateUserComponent extends Component {
+
+  @service firebaseApp;
   @service store;
-  @tracked name;
-  @tracked pass;
-  @tracked passRepeat;
 
-@action createUser() {
-    if (this.pass === this.passRepeat) {
-      let newUser = this.store.createRecord("user", {
-        name: this.name,
-        password: this.pass,
+  @tracked email = '';
+  @tracked password = '';
+
+  @action createUser() {
+    this.firebaseApp.auth().then((auth) => {
+      auth.createUserWithEmailAndPassword(this.email, this.password).then(({user}) => {
+        const user1 = this.store.createRecord('user', {
+          email: user.email
+        });
+        set(user1, 'id', user.uid)
+        user1.save();
       });
-      newUser.save();
-
-      this.name = '';
-      this.pass = '';
-      this.passRepeat = '';
-    }
+    })
   }
 }
--- app/components/log-in.js
+++ app/components/log-in.js
@@ -4,20 +4,23 @@ import {tracked} from "@glimmer/tracking";
 import { inject as service } from '@ember/service';
 
 export default class LogInComponent extends Component {
-  @tracked name;
-  @tracked pass;
-  @service store;
+  @tracked email;
+  @tracked password;
+  @service firebaseApp;
 
   @action async accessToProfile() {
-    const data = await this.store.findRecord("user", "T7c8HHdeGlnoPPnhChze");
-    if (this.name === data.name && this.pass === data.password) {
-      console.log("Done.");
-    } else {
-      console.log("Failed.");
-    }
-    console.log("attempt Name: " + this.name);
-    console.log("firebase Name: " + data.name);
-    console.log("attempt Pass: " + this.pass);
-    console.log("firebase Pass: " + data.password);
+    this.firebaseApp
+    .auth()
+    .then((auth) => {
+      auth.signInWithEmailAndPassword(this.email, this.password).then((user) => {
+        console.log(user);
+        this.email = '';
+        this.password = '';
+        // this.setProperties({
+          // email: '',
+          // password: ''
+        // });
+      })
+    })
   }
 }
